840eae12968f2ec42904bb1867b58fd32c010f9c
nmduong99 <huyenlinh1235@gmail.com>
2020-04-02 15:50:09 +0700

    update

--- recorder.py
+++ recorder.py
@@ -1,9 +1,33 @@
-import sounddevice
-from scipy.io.wavfile import write
+import numpy as np
+import sounddevice as sd
+import soundfile as sf
+import time
+import queue
 
-fs = 44100
-secs = 10
-print('recording...')
-recordVoice = sounddevice.rec(int(secs * fs), samplerate = fs, channels = 2)
-sounddevice.wait();
-write("output.wav", fs, recordVoice)
+sd.default.device = 0
+FS = 16000
+CHANNELS = 1
+
+q = queue.Queue()
+
+def audioCallback(inData, frames, time, status):
+    if status:
+        print(status)
+    q.put(inData.copy())
+
+def main():
+    try:
+        data = np.array([])
+        with sd.InputStream(samplerate = FS, channels = CHANNELS, callback = audioCallback):
+            print('Recording...')
+            print('Press Ctrl + C to stop recording!')
+            while True:
+                data = np.append(data,q.get())
+    except KeyboardInterrupt:
+       print('Stop recording')
+       filename = input('Enter filename to save record: ')
+       sf.write(filename + '.wav', data, FS)
+       print('Save successfully')
+
+if __name__ == '__main__':
+    main()
